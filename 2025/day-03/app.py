8d989a6dae89b0022dfdb9ff65750ff291c8fd15
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2025-12-05 21:59:56 +0000

    feat: solve part 1

--- 2025/day-03/app.py
+++ 2025/day-03/app.py
@@ -46,7 +46,7 @@ def find_first_battery_with_highest_joltage(bank: Bank, /) -> IndexedBattery:
     index: int
     battery: Battery
     for index, battery in enumerate(bank[1:]):
-        if battery < highest_battery:
+        if battery <= highest_battery:
             continue
 
         highest_index = index + 1
@@ -57,19 +57,6 @@ def find_first_battery_with_highest_joltage(bank: Bank, /) -> IndexedBattery:
 
 def find_primary_battery(bank: Bank, /) -> IndexedBattery:
     return find_first_battery_with_highest_joltage(bank[:-1])
-    # primary_index: int = 0
-    # primary_battery: Battery = bank[0]
-
-    # index: int
-    # battery: Battery
-    # for index, battery in enumerate(bank[1:-1]):
-    #     if battery < primary_battery:
-    #         continue
-
-    #     primary_index = index
-    #     primary_battery = battery
-
-    # return IndexedBattery(primary_battery, primary_index)
 
 
 def find_secondary_battery(
@@ -79,9 +66,6 @@ def find_secondary_battery(
         primary_battery_index = find_primary_battery(bank).index
 
     index_offset: int = primary_battery_index + 1
-    print("\tPrimary Battery Index:", primary_battery_index)
-    print("\tIndex Offset:", index_offset)
-    print("\tSub-Bank:", "".join(map(str, bank[index_offset:])))
     secondary_battery: IndexedBattery = find_first_battery_with_highest_joltage(
         bank[index_offset:]
     )
@@ -90,29 +74,21 @@ def find_secondary_battery(
         secondary_battery.battery, index_offset + secondary_battery.index
     )
 
-    # secondary_index: int = primary_battery_index + 1
-    # secondary_battery: Battery = bank[secondary_index]
 
-    # offset: int
-    # battery: Battery
-    # for offset, battery in enumerate(bank[secondary_index+1:]):
-    #     if battery < secondary_battery:
-    #         continue
-
-    #     secondary_index = secondary_index + offset
-    #     secondary_battery = battery
-
-    # return IndexedBattery(secondary_battery, secondary_index)
-
-
-bank: Bank
-for bank in read_input():
+def calc_max_bank_joltage(bank: Bank, /) -> int:
     primary_battery: IndexedBattery = find_primary_battery(bank)
     secondary_battery: IndexedBattery = find_secondary_battery(
         bank, primary_battery_index=primary_battery.index
     )
 
-    print("Bank:", "".join(map(str, bank)))
-    print("Primary:", primary_battery)
-    print("Secondary:", secondary_battery)
-    break
+    return int_join(primary_battery.battery, secondary_battery.battery)
+
+
+def solve_part_1() -> int:
+    return sum(calc_max_bank_joltage(bank) for bank in read_input())
+
+
+### Part 1 ###
+part_1: int = solve_part_1()
+print("Part 1:", part_1)
+assert part_1 == 17095
